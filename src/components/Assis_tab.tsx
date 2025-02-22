63f4dc658c6e35d0a7fefce164667d4ed9904ef9
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-02-22 20:14:46 +0530

    admin assis

---
+++ src/components/Assis_tab.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Assis_tab = () => {
+  return <div>Assis_tab</div>;
+};
+
+export default Assis_tab;
---
+++ src/components/UpdatePat.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdatePat = () => {
+  return <div>UpdatePat</div>;
+};
+
+export default UpdatePat;
